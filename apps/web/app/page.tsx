b7d3e5140c5d8545b40ea6cca19bf1185bbf183f
rishi-xyz <itsranahrishikesh@gmail.com>
2025-02-05 15:30:52 +0530

    Added shadcn ui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,10 @@
+import {Button} from "@repo/ui/components"
+
 export default function MarketingPage(){
   return(
     <div>
       <h2 className="text-2xl">Marketing page</h2>
+      <Button >Hello</Button>
     </div>
   );
 }
