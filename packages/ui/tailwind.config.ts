a31374d77379f5183878485dc92da8d865fd8fbe
rishi-xyz <itsranahrishikesh@gmail.com>
2025-02-05 14:51:21 +0530

    Added and modified tailwind

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
-  presets: [sharedConfig],
-};
-
-export default config;
+export default {
+  ...sharedConfig,
+  content:[
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ]
+} satisfies Config;
